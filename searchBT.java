cc41ba507087b863de2f49efa75af6d145fcd87f
michaelcolleran1 <144271462+michaelcolleran1@users.noreply.github.com>
2023-10-04 14:50:39 -0400

    final

--- searchBT.java
+++ searchBT.java
@@ -2,22 +2,23 @@ import org.w3c.dom.Node;
 
 public class searchBT extends BinaryTree {
 	
-	public boolean contains(Node node, int item) {
+	public boolean contains(BinaryNode node, int item) {
 		if(node == null) {
 			return false;
 		} else {
-			if(node.data.compareTo(item) == 0) {
+			if(node.data == item) {
 					return true;
 				} else {
-					return contains(node.left, item) || contains(node.right, item);
+					return contains(node.left.data, item) || contains(node.right.data, item);
 			}
 		}
 	}
 
-	public int getLevel(int item) {
-		if (BinaryTree.data == null) {
+	public int getLevel(BinaryNode node, int item) {
+		if (node == null) {
 			return -1;
 		} else {
+			return Math.max(getLevel(node.left), getLevel(node.right)) + 1;
 			
 		}
 	}
